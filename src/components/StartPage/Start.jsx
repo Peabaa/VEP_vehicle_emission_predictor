3a9abc1b52b9693c856c45804f852359649e68fc
Peabaa <mdpastor14@gmail.com>
2024-12-07 00:27:25 +0800

    add: finished start UI

--- src/components/StartPage/Start.jsx
+++ src/components/StartPage/Start.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
-import Car from "../../assets/carPic.png"
+import Car from "../../assets/carPic4.png"
+import Logo from "../../assets/vep logo.png"
 import "./Start.css"
 
 const Start = () => {
   return (
     <div className="start-container">
-      <button className="start-button">Start</button>
-      <img src={Car} alt="Car 1" className="carPic1" />
+      <div className="content">
+        <div className="text-overlay">VEHICLE EMISSION<br />PREDICTOR</div>
+        <div className="image-container">
+          <img src={Car} alt="Car" className="carPic1" />
+        </div>
+          <img src={Logo} alt="VEP Logo" className="logoPic" />
+        <button className="start-button">Start</button>
+      </div>
     </div>
   );
 }
